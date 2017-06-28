9a35694991f5be3e56c169810d134e1dedd4b942
Adrian Röfer <aroefer@uni-bremen.de>
2017-06-28 17:38:56 +0200

    :muscle: New shortcuts

--- action_shortcuts.bash
+++ action_shortcuts.bash
@@ -1,15 +1,34 @@
 alias basepose=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_upper_body_joint_control.yaml\ config/pr2_upper_body.yaml\ test_params/upper_body_praying_mantis.yaml\ feedback
-alias rgrasp=rosrun\ suturo_action_server\ client_test\ controller_specs/gripper_control.yaml\ config/pr2_right_gripper.yaml\ test_params/grasp_r_50.yaml\ feedback
-alias lgrasp=rosrun\ suturo_action_server\ client_test\ controller_specs/gripper_control.yaml\ config/pr2_left_gripper.yaml\ test_params/grasp_l_50.yaml\ feedback
+alias lbasepose=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_left_arm_joint_control.yaml\ config/pr2_upper_body.yaml\ test_params/upper_body_praying_mantis.yaml\ feedback
+alias rgrasp=rosrun\ suturo_action_server\ client_test\ controller_specs/gripper_control_r.yaml\ config/pr2_right_gripper.yaml\ test_params/grasp_r_50.yaml\ feedback
+alias lgrasp=rosrun\ suturo_action_server\ client_test\ controller_specs/gripper_control_l.yaml\ config/pr2_left_gripper.yaml\ test_params/grasp_l_50.yaml\ feedback
 alias rrelease=rosrun\ suturo_action_server\ client_test\ controller_specs/gripper_control.yaml\ config/pr2_right_gripper.yaml\ test_params/release_r_50.yaml\ feedback
 alias lrelease=rosrun\ suturo_action_server\ client_test\ controller_specs/gripper_control.yaml\ config/pr2_left_gripper.yaml\ test_params/release_l_50.yaml\ feedback
 alias rapproach=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_grasp_control_r.yaml\ config/pr2_upper_body_right_arm.yaml\ test_params/approach_cylinder_r.yaml\ feedback
 alias lapproach=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_grasp_control_l.yaml\ config/pr2_upper_body_left_arm.yaml\ test_params/approach_cylinder_l.yaml\ feedback
-alias rplace=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_place_control_r.yaml\ config/pr2_upper_body_right_arm.yaml\ test_params/place_cylinder_r.yaml\ feedback
-alias lplace=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_place_control_l.yaml\ config/pr2_upper_body_left_arm.yaml\ test_params/place_cylinder_l.yaml\ feedback
+alias rplacecy=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_place_control_r.yaml\ config/pr2_upper_body_right_arm.yaml\ test_params/place_cylinder_r.yaml\ feedback
+alias lplacecy=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_place_control_l.yaml\ config/pr2_upper_body_left_arm.yaml\ test_params/place_cylinder_l.yaml\ feedback
 alias ra_plate=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_grasp_plate_r.yaml\ config/pr2_upper_body_right_arm.yaml\ test_params/approach_plate_r.yaml\ feedback
 alias selfcoll=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_self_collision_test.giskard\ config/pr2_upper_body.yaml\ test_params/self_collision_test.yaml\ feedback
-alias cutpos_r=rosrun\ suturo_action_server\ client_test\ controller_specs/cut_position.yaml\ config/pr2_upper_body_right_arm.yaml\ test_params/cut_pos.yaml\ feedback
-alias cut_r=rosrun\ suturo_action_server\ client_test\ controller_specs/cut.yaml\ config/pr2_upper_body_right_arm.yaml\ test_params/cut_pos.yaml\ feedback
+alias cutpos_r=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_cut_position_r.giskard\ config/pr2_upper_body_right_arm.yaml\ test_params/cut_pos.yaml\ feedback
+alias cut_r=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_cut_r.yaml\ config/pr2_upper_body_right_arm.yaml\ test_params/cut.yaml\ feedback
+alias pushcake_r=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_push_cake_r.giskard\ config/pr2_upper_body_right_arm.yaml\ test_params/push_r.yaml\ feedback
 
-alias rsimple_collision=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_r_arm_collision_avoidance.giskard\ config/pr2_upper_body_right_arm.yaml\ test_params/r_arm_collision_test.yaml\ feedback
+alias knife_grasp_r=rosrun\ suturo_action_server\ client_test\ controller_specs/knife_grasp_r.yaml\ config/pr2_upper_body_right_arm.yaml\ test_params/knife_grasp.yaml\ feedback
+alias knife_grasp_l=rosrun\ suturo_action_server\ client_test\ controller_specs/knife_grasp_l.yaml\ config/pr2_upper_body_right_arm.yaml\ test_params/knife_grasp.yaml\ feedback
+
+alias detachr=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_detach_knife_r.giskard\ config/pr2_upper_body_right_arm.yaml\ test_params/detach_r.yaml\ feedback
+
+alias movecaker=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_move_cake_r.giskard\ config/pr2_upper_body_right_arm.yaml\ test_params/move_cake_r.yaml\ feedback
+
+alias bullshit=rosrun\ suturo_action_server\ client_test\ controller_specs/torso_pos.giskard\ config/pr2_torso.yaml\ test_params/torso_position.yaml\ feedback
+alias nodding=rosrun\ suturo_action_server\ client_test\ controller_specs/nodding.giskard\ config/pr2_head.yaml\ test_params/nod_position.yaml\ feedback
+alias poi=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_look_at.giskard\ config/pr2_lookAt_joints.yaml\ test_params/poi_test.yaml\ feedback
+alias rgraspplate=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_grasp_plate_r.giskard\ config/pr2_upper_body_right_arm.yaml\ test_params/plate_grasp.yaml\ feedback
+alias lgraspplate=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_grasp_plate_l.giskard\ config/pr2_upper_body_right_arm.yaml\ test_params/plate_grasp.yaml\ feedback
+alias rmoveandflip=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_move_and_flip_r.giskard\ config/pr2_upper_body_right_arm.yaml\ test_params/move_and_flip_r.yaml\ feedback
+alias lmoveandflip=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_move_and_flip_l.giskard\ config/pr2_upper_body_left_arm.yaml\ test_params/move_and_flip_l.yaml\ feedback
+alias rplacespat=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_place_control_r.yaml\ config/pr2_upper_body_right_arm.yaml\ test_params/place_spatula_r.yaml\ feedback
+alias lplacespat=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_place_control_l.yaml\ config/pr2_upper_body_right_arm.yaml\ test_params/place_spatula_l.yaml\ feedback
+alias rgraspspat=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_grasp_fingerHandle_r.giskard\ config/pr2_upper_body_right_arm.yaml\ test_params/grasp_spatula.yaml\ feedback
+alias lgraspspat=rosrun\ suturo_action_server\ client_test\ controller_specs/pr2_grasp_fingerHandle_l.giskard\ config/pr2_upper_body_right_arm.yaml\ test_params/grasp_spatula.yaml\ feedback
