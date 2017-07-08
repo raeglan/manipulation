10aeb0c8ce26f267f0a504104c1ca47fef34fc21
Adrian Röfer <aroefer@uni-bremen.de>
2017-07-08 21:08:04 +0200

    :bug: Finally fixed rviz crash on shutdown when using sim panel

--- suturo_action_tester/src/SuturoSimPanel.cpp
+++ suturo_action_tester/src/SuturoSimPanel.cpp
@@ -21,40 +21,40 @@ namespace SuturoSim {
 
 SuturoSimPanel::SuturoSimPanel(QWidget* pParent) 
 : rviz::Panel(pParent)
-, rootProperty("PanelRootProperty")
-, topicsProperty("Topics", QVariant(), QString(), &rootProperty)
-, simTopicProperty("Simulation Topic", "/suturo/create_object", "suturo_action_tester/SuturoCreateObject", "Topic on which to connect to the simulation", &topicsProperty, SLOT(updateTopic()), this)
-, saveTopicProperty("Simulation Save Topic", "/suturo/save_scene", "std_msgs/String", "Topic on which to connect to the simulation", &topicsProperty, SLOT(updateTopic()), this)
-, loadTopicProperty("Simulation Load Topic", "/suturo/load_scene", "std_msgs/String", "Topic on which to connect to the simulation", &topicsProperty, SLOT(updateTopic()), this)
-
-, objRootProperty("Object", QVariant(), QString(), &rootProperty)
-, objNameProperty("Object Name", "object1", "Name of the object to create", &objRootProperty, SLOT(updateVisMarker()), this)
-, objColorProperty("Color", QColor(204,51,204), "Color of the marker", &objRootProperty, SLOT(updateVisMarker()), this)
-, objAlphaProperty("Alpha", 1.0f, "Alpha of the marker", &objRootProperty, SLOT(updateVisMarker()), this)
-, objShapeProperty("Shape", "Cube", "Shape of the new object", &objRootProperty, SLOT(updateVisMarker()), this)
-, objScaleProperty("Scale", Ogre::Vector3(1,1,1), "Scale of the object", &objRootProperty, SLOT(updateVisMarker()), this)
-, objMeshProperty("Mesh", "", "Mesh for the object. First part of path is ros-package", &objRootProperty, SLOT(updateVisMarker()), this)
 , objFrameProperty(0)
-, scenePathProperty("Scene Path", "", "Path to save the scene to and load it from", &rootProperty)
 , bSimCmdConnected(false)
 , bSimSaveConnected(false) 
 , bSimLoadConnected(false)
 {
+	rootProperty      = new rviz::Property("PanelRootProperty");
+	topicsProperty    = new rviz::Property("Topics", QVariant(), QString(), rootProperty);
+	simTopicProperty  = new rviz::RosTopicProperty("Simulation Topic", "/suturo/create_object", "suturo_action_tester/SuturoCreateObject", "Topic on which to connect to the simulation", topicsProperty, SLOT(updateTopic()), this);
+	saveTopicProperty = new rviz::RosTopicProperty("Simulation Save Topic", "/suturo/save_scene", "std_msgs/String", "Topic on which to connect to the simulation", topicsProperty, SLOT(updateTopic()), this);
+	loadTopicProperty = new rviz::RosTopicProperty("Simulation Load Topic", "/suturo/load_scene", "std_msgs/String", "Topic on which to connect to the simulation", topicsProperty, SLOT(updateTopic()), this);
+	objRootProperty   = new rviz::Property("Object", QVariant(), QString(), rootProperty);
+	objNameProperty   = new rviz::StringProperty("Object Name", "object1", "Name of the object to create", objRootProperty, SLOT(updateVisMarker()), this);
+	objColorProperty  = new rviz::ColorProperty("Color", QColor(204,51,204), "Color of the marker", objRootProperty, SLOT(updateVisMarker()), this);
+	objAlphaProperty  = new rviz::FloatProperty("Alpha", 1.0f, "Alpha of the marker", objRootProperty, SLOT(updateVisMarker()), this);
+	objShapeProperty  = new rviz::EnumProperty("Shape", "Cube", "Shape of the new object", objRootProperty, SLOT(updateVisMarker()), this);
+	objScaleProperty  = new rviz::VectorProperty("Scale", Ogre::Vector3(1,1,1), "Scale of the object", objRootProperty, SLOT(updateVisMarker()), this);
+	objMeshProperty   = new rviz::StringProperty("Mesh", "", "Mesh for the object. First part of path is ros-package", objRootProperty, SLOT(updateVisMarker()), this);
+	scenePathProperty = new rviz::StringProperty("Scene Path", "", "Path to save the scene to and load it from", rootProperty);
+
 	QVBoxLayout* masterLayout = new QVBoxLayout;
 
-	objAlphaProperty.setMin(0.f);
-	objAlphaProperty.setMax(1.f);
+	objAlphaProperty->setMin(0.f);
+	objAlphaProperty->setMax(1.f);
 
-	objShapeProperty.addOption("Cube", visualization_msgs::Marker::CUBE);
-	objShapeProperty.addOption("Sphere", visualization_msgs::Marker::SPHERE);
-	objShapeProperty.addOption("Cylinder", visualization_msgs::Marker::CYLINDER);
-	objShapeProperty.addOption("Mesh", visualization_msgs::Marker::MESH_RESOURCE);
+	objShapeProperty->addOption("Cube", visualization_msgs::Marker::CUBE);
+	objShapeProperty->addOption("Sphere", visualization_msgs::Marker::SPHERE);
+	objShapeProperty->addOption("Cylinder", visualization_msgs::Marker::CYLINDER);
+	objShapeProperty->addOption("Mesh", visualization_msgs::Marker::MESH_RESOURCE);
 
-	objPTree = new rviz::PropertyTreeModel(&rootProperty, this);
+	objPTree = new rviz::PropertyTreeModel(rootProperty, this);
 
 	QVBoxLayout* markerLayout = new QVBoxLayout;
  	rviz::PropertyTreeWidget* ptWidget = new rviz::PropertyTreeWidget;
- 	ptWidget->setModel(rootProperty.getModel());
+ 	ptWidget->setModel(rootProperty->getModel());
  	markerLayout->addWidget(ptWidget);
 
  	QPushButton* btnCreateObject = new QPushButton("Create Object", this);
@@ -79,43 +79,43 @@ SuturoSimPanel::SuturoSimPanel(QWidget* pParent)
 void SuturoSimPanel::onInitialize() {
 	rviz::Panel::onInitialize();
 
-	objFrameProperty = new rviz::TfFrameProperty("Frame", "odom_combined", "Reference frame for the object", &objRootProperty, vis_manager_->getFrameManager(), false, SLOT(updateVisMarker()), this);	
+	objFrameProperty = new rviz::TfFrameProperty("Frame", "odom_combined", "Reference frame for the object", objRootProperty, vis_manager_->getFrameManager(), false, SLOT(updateVisMarker()), this);	
 }
 
 void SuturoSimPanel::updateTopic() {
-	setCmdTopic(simTopicProperty.getString());
-	setSaveTopic(saveTopicProperty.getString());
-	setLoadTopic(loadTopicProperty.getString());
+	setCmdTopic(simTopicProperty->getString());
+	setSaveTopic(saveTopicProperty->getString());
+	setLoadTopic(loadTopicProperty->getString());
 }
 
 void SuturoSimPanel::updateVisMarker() {
-	visMarker.header.frame_id = objNameProperty.getStdString();
+	visMarker.header.frame_id = objNameProperty->getStdString();
 
-	if (objShapeProperty.getOptionInt() == visualization_msgs::Marker::MESH_RESOURCE) {
-		if (objMeshProperty.getHidden()) {
-			objMeshProperty.show();
+	if (objShapeProperty->getOptionInt() == visualization_msgs::Marker::MESH_RESOURCE) {
+		if (objMeshProperty->getHidden()) {
+			objMeshProperty->show();
 		}
-		visMarker.mesh_resource = "package://" + objMeshProperty.getStdString();
-	} else if (!objMeshProperty.getHidden()) {
-		objMeshProperty.hide();
+		visMarker.mesh_resource = "package://" + objMeshProperty->getStdString();
+	} else if (!objMeshProperty->getHidden()) {
+		objMeshProperty->hide();
 	}
 
-	QColor color = objColorProperty.getColor();
+	QColor color = objColorProperty->getColor();
 
 	visMarker.action = visualization_msgs::Marker::ADD;
-	visMarker.type = objShapeProperty.getOptionInt();
+	visMarker.type = objShapeProperty->getOptionInt();
 
 	visMarker.pose.orientation.x = 0;
 	visMarker.pose.orientation.y = 0;
 	visMarker.pose.orientation.z = 0;
 	visMarker.pose.orientation.w = 1;
 
-	Ogre::Vector3 scale = objScaleProperty.getVector();
+	Ogre::Vector3 scale = objScaleProperty->getVector();
 	visMarker.scale.x = scale.x;
 	visMarker.scale.y = scale.y;
 	visMarker.scale.z = scale.z;
 
-	visMarker.color.a = objAlphaProperty.getFloat();
+	visMarker.color.a = objAlphaProperty->getFloat();
 	visMarker.color.r = color.redF();
 	visMarker.color.g = color.greenF();
 	visMarker.color.b = color.blueF();
@@ -170,7 +170,7 @@ void SuturoSimPanel::setLoadTopic(const QString& topic) {
 
 void SuturoSimPanel::createObject() {
 	suturo_action_tester::SuturoCreateObject msg;
-	msg.name = objNameProperty.getStdString();
+	msg.name = objNameProperty->getStdString();
 	msg.refFrame = objFrameProperty->getStdString();
 	msg.vis = visMarker;
 	if (bSimCmdConnected)
@@ -179,28 +179,28 @@ void SuturoSimPanel::createObject() {
 
 void SuturoSimPanel::save(rviz::Config config) const {
 	rviz::Panel::save(config);
-	rootProperty.save(config);
+	rootProperty->save(config);
 }
 
 void SuturoSimPanel::load(const rviz::Config& config) {
 	rviz::Panel::load(config);
-	rootProperty.load(config);
+	rootProperty->load(config);
 	updateVisMarker();
 	updateTopic();
 }
 
 void SuturoSimPanel::loadScene() {
-	if (bSimLoadConnected && scenePathProperty.getString() != "") {
+	if (bSimLoadConnected && scenePathProperty->getString() != "") {
 		std_msgs::String msg;
-		msg.data = scenePathProperty.getStdString();
+		msg.data = scenePathProperty->getStdString();
 		simLoadPublisher.publish(msg);
 	}
 }
 
 void SuturoSimPanel::saveScene() {
-	if (bSimSaveConnected && scenePathProperty.getString() != "") {
+	if (bSimSaveConnected && scenePathProperty->getString() != "") {
 		std_msgs::String msg;
-		msg.data = scenePathProperty.getStdString();
+		msg.data = scenePathProperty->getStdString();
 		simSavePublisher.publish(msg);
 	}
 }
