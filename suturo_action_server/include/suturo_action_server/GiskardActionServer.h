3c44b573f96ec83405ecd64c87555c56400b6724
Adrian Röfer <aroefer@uni-bremen.de>
2017-09-05 16:05:10 +0200

    :memo: Documentation and thinning of action server

--- suturo_action_server/include/suturo_action_server/GiskardActionServer.h
+++ suturo_action_server/include/suturo_action_server/GiskardActionServer.h
@@ -38,7 +38,8 @@ public:
 
 	/**
 	 * @brief      Constructor for giskard action server. 
-	 * Constructor for giskard action server. During construction, an actionlib action server is created and command and visualization topics are advertised. Additional configuration information is read from the parameter ~config on the parameter server.
+	 * 
+	 * During construction, an actionlib action server is created and command and visualization topics are advertised. Additional configuration information is read from the parameter ~config on the parameter server.
 	 * 
 	 * @param[in]  _name  The name that the actionlib action server will be created with
 	 */
@@ -53,7 +54,8 @@ public:
 	
 	/**
 	 * @brief      Configures the action server using a yaml configuration. 
-	 * Configures the action server using a yaml configuration. The yaml structure must be a map.
+	 * 
+	 * The yaml structure must be a map.
 	 * The map is scanned for the following keys:
 	 * 	- position_controllers: Defines which joints are supposed be controlled using position commands  
 	 * 	- gripper_controllers: Defines whoch joints should be controlled as grippers
@@ -71,6 +73,7 @@ public:
 
 	/**
 	 * @brief      Performs one controller update cycle
+	 * 
 	 * This function performs one controller update. It evaluates all parameter queries, generates and sends new commands and updates the visualization.
 	 * @param[in]  jointState  The current joint state
 	 */
@@ -108,82 +111,171 @@ public:
 	 */
 	bool decodeVector(const string& name, Eigen::Vector3d vector);
 	
-	
+	/**
+	 * @brief      Decodes a transform parameter from a string and writes it to the state vector.
+	 * @param[in]  name   Name of the corresponding input in the current controller
+	 * @param[in]  vector  Transform encoded as string: "X Y Z AX AY AZ Angle"
+	 * @return     Success of decoding and value assignment.
+	 */
 	bool decodeTransform(const string& name, string transform);
 	
+	/**
+	 * @brief      Assigns a transform parameter to the state vector.
+	 * @param[in]  name   Name of the corresponding input in the current controller
+	 * @param[in]  vector  Transform
+	 * @return     Success of value assignment.
+	 */
 	bool decodeTransform(const string& name, tf::Transform transform);
 
 
 protected:
+	/** Iteration limit for giskard per update cycle */
 	int nWSR;
+
+	/** Flag set to terminate controller execution from deeper layers */
 	bool terminateExecution;
-	double rGripperEffort, lGripperEffort;
-	int lGripperIdx, rGripperIdx;
+
+	/** Name of the action server */
 	const string name;
+
+	/** Server's node handle */
 	ros::NodeHandle nh;
+
+	/** Action server instance */
 	actionlib::SimpleActionServer<suturo_manipulation_msgs::MoveRobotAction> server;
 
+	/** Current generated controller */
 	giskard_core::QPController controller;
+	
+	/** Controller state vector */
 	Eigen::VectorXd state;
+
+	/** Map of all velocity controllers */
 	map<std::string, ros::Publisher> velControllers;
 	
+	/**
+	 * @brief      Internal structure to manage a position controller.
+	 */
 	struct posController {
 		ros::Publisher pub;
 		double dT;
 	};
+
+	/** Map of joints being controlled with position controllers */
 	unordered_map<string, posController> posControllers;
 
+	/**
+	 * @brief      Internal structure to manage a gripper controller. 
+	 */
 	struct gripperController {
 		ros::Publisher pub;
 		double effort;
 		double dT;
 	}; 
+
+	/** Map of joints being controlled with gripper controllers */
 	unordered_map<string, gripperController> gripperControllers;
 
+	/** Set containing the names of all joints referenced in current controller */
 	unordered_set<string> jointSet;
 
+	/** List of queries to run during update cycle */
 	vector<boost::shared_ptr<AQuery>> queries;
 
+	/** Map of last velocity commands */
 	map<string, double> lastCommand;
+
+	/** Map of last position commands */
 	map<string, double> lastControllablePos;
+
+	/** Time of last update cycle */
 	ros::Time lastUpdate;
 
+	/** Name of origin frame for visualization */
 	string visRefFrameName;
 private:
+	/** Mutex used to allow for asynchronous joint state updates */
 	mutex jsMutex;
 
+	/**
+	 * @brief      Parses controller scope for values to visualize.
+	 *
+	 * Values beginning with "VIS__" will be visualized. Their visual name is determined by the part following the prefix. If a vector should be visualized as direction, the name of a base point must be provided. This point's name is separated from the visual name with a double underscore "__".  
+	 *
+	 * @param[in]  scope  The scope
+	 */
 	void generateVisualsFromScope(const giskard_core::Scope& scope);
 	
+	/** Flag showing whether a new joint state has been received since last update cycle */
 	bool newJS;
+
+	/** Last received joint state */
 	sensor_msgs::JointState currentJS;
 
-	double dT, rGripper_dT, lGripper_dT;
+	/** Seconds passed since last update cycle */
+	double dT;
+
+	/** Tranform listener used by Tf queries */
 	tf::TransformListener tfListener;
-	double lastFeedback;
-	ros::Subscriber jsSub;
-	bool controllerInitialized;
-	KDL::Expression<double>::Ptr feedbackExpr;
-	ros::Publisher rGripperPub, lGripperPub;
-	ros::Subscriber rGripperSub, lGripperSub;
 	
+	/** Value of feedback after last update cycle */
+	double lastFeedback;
+
+	/** Joint state subscriber */
+	ros::Subscriber jsSub;
+
+	/** Flag showing whether current controller was initialized */
+	bool controllerInitialized;
+
+	/** Pointer to feedback expression */
+	KDL::Expression<double>::Ptr feedbackExpr;
+	
+	/** Type definition for vector visualization. */
 	typedef std::pair<KDL::Expression<KDL::Vector>::Ptr, KDL::Expression<KDL::Vector>::Ptr> TVecPair;
+	
+	/** Scalars to visualize */
 	unordered_map<string, KDL::Expression<double>::Ptr> visScalars;
+	
+	/** Points to visualize */
 	unordered_map<string, KDL::Expression<KDL::Vector>::Ptr> visPoints;
+	
+	/** Vectors to visualize */
 	unordered_map<string, TVecPair> visVectors;
+	
+	/** Frames to visualize */
 	unordered_map<string, KDL::Expression<KDL::Frame>::Ptr> visFrames;
 
+	/** Publisher for joint state commands */
 	ros::Publisher jsCmdPub;
+
+	/** Publishers for deviations of measurements and expectations */
 	ros::Publisher posErrorPub, velErrorPub;
+
+	/** Publishers for visualization */
 	ros::Publisher visPub, visScalarPub;
+
+	/** Visualization manager to manage Ids and deletion of markers */
 	VisualizationManager visManager;
 
+	/** Collision Scene */
 	CollisionScene collisionScene;
+	
+	/** Results of collision scene updates */
 	CollisionScene::QueryMap collQueryMap;
 
+	/** Action server feedback message */
 	suturo_manipulation_msgs::MoveRobotFeedback feedback;
+	
+	/** Action server result message */
 	suturo_manipulation_msgs::MoveRobotResult result;
 };
 
+
+/**
+ * @brief      Abstract base class for all action server queries
+ * 
+ * This abstract base class provides the data and function declarations the action server needs from a query to run it.
+ */
 struct AQuery {
 	AQuery(GiskardActionServer* _pServer, string _name)
 	: pServer(_pServer)
@@ -194,15 +286,22 @@ struct AQuery {
 
 	virtual bool eval() = 0;
 protected:
+	/** Name of the controller input */
 	const string name;
+
+	/** Pointer to the action server */
 	GiskardActionServer* pServer;
 };
 
+
+/**
+ * @brief      Query looking up a transformation in Tf.
+ */
 struct TFQuery : public AQuery {
-	TFQuery(GiskardActionServer* pS, string name, string _frameId, string _refFrame, tf::TransformListener* _tfListener) 
+	TFQuery(GiskardActionServer* pS, string name, string targetFrame, string sourceFrame, tf::TransformListener* _tfListener) 
 	: AQuery(pS, name)
-	, frameId(_frameId)
-	, refFrame(_refFrame)
+	, frameId(targetFrame)
+	, refFrame(sourceFrame)
 	, tfListener(_tfListener)
 	{
 		assert(tfListener);
@@ -224,11 +323,20 @@ struct TFQuery : public AQuery {
 	}
 
 private:
+	/** Target frame */
 	const string frameId;
+
+	/** Source frame */
 	const string refFrame;
+	
+	/** Pointer to transform listener used for lookup */
 	const tf::TransformListener* tfListener;
 };
 
+
+/**
+ * @brief      Query assigning the time passed since the start of the controller to an input.
+ */
 struct ElapsedTimeQuery : public AQuery {
 	ElapsedTimeQuery(GiskardActionServer* _pServer, string _name)
 	: AQuery(_pServer, _name) 
@@ -243,9 +351,14 @@ struct ElapsedTimeQuery : public AQuery {
 	}
 
 private:
+	/** Time when controller started */
 	const ros::Time start;
 };
 
+
+/**
+ * @brief      Query updating the closest points between a robot link and the surroundings.
+ */
 struct CollisionQuery : public AQuery {
 	CollisionQuery(GiskardActionServer* _pServer, string _link, CollisionScene::QueryMap& _map)
 	: AQuery(_pServer, "Collision:"+_link) 
@@ -268,7 +381,12 @@ struct CollisionQuery : public AQuery {
 	}
 
 private:
+	/** Name of the link */
 	const string link;
+
+	/** Names of the inputs */
 	const string on_link, in_world;
+	
+	/** Result map of collision scene */
 	CollisionScene::QueryMap& map;
 };
